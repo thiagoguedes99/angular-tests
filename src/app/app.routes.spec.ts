38b37400233862c6eaf61baec2bf34fa8c2bd24c
Thiago Guedes <tguedes6@hotmail.com>
2023-12-05 17:48:31 -0300

    feat: update tests in appRouter

--- src/app/app.routes.spec.ts
+++ src/app/app.routes.spec.ts
@@ -1,4 +1,5 @@
 import { routes } from './app.routes'
+import { CounterRouteComponent } from './basic/counter-route/counter-route.component';
 import { CounterComponent } from './basic/counter/counter.component';
 import { FatherComponent } from './basic/family/father/father.component';
 import { PokemonComponent } from './basic/pokemon/pokemon.component';
@@ -11,7 +12,7 @@ describe('AppRoutes', () => {
   });
 
   it('should to length routes', () => {
-    expect(routes.length).toBe(3);
+    expect(routes.length).toBe(4);
   });
 
   it('should to router for path basic/counter the CounterComponent', () => {
@@ -19,14 +20,19 @@ describe('AppRoutes', () => {
     expect(routes[0].component).toBe(CounterComponent);
   });
 
+  it('should to router for path basic/counter/:initial the CounterComponent', () => {
+    expect(routes[1].path).toBe('basic/counter/:initial');
+    expect(routes[1].component).toBe(CounterRouteComponent);
+  });
+
   it('should to router for path basic/pokemmon the PokemonComponent', () => {
-    expect(routes[1].path).toBe('basic/pokemmon');
-    expect(routes[1].component).toBe(PokemonComponent);
+    expect(routes[2].path).toBe('basic/pokemmon');
+    expect(routes[2].component).toBe(PokemonComponent);
   });
 
   it('should to router for path basic/family the PokemonComponent', () => {
-    expect(routes[2].path).toBe('basic/family');
-    expect(routes[2].component).toBe(FatherComponent);
+    expect(routes[3].path).toBe('basic/family');
+    expect(routes[3].component).toBe(FatherComponent);
   });
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { CounterComponent } from './basic/counter/counter.component';
 import { PokemonComponent } from './basic/pokemon/pokemon.component';
 import { FatherComponent } from './basic/family/father/father.component';
+import { CounterRouteComponent } from './basic/counter-route/counter-route.component';
 
 export const routes: Routes = [
   {
     path: 'basic/counter',
     component: CounterComponent
+  },
+  {
+    path: 'basic/counter/:initial',
+    component: CounterRouteComponent
   },
   {
     path: 'basic/pokemmon',
