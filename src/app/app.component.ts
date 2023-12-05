00a33ec7d778264248fc627db30e246213bb8518
Thiago Guedes <tguedes6@hotmail.com>
2023-12-05 17:47:08 -0300

    feat: update AppComponent with buttons redirects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   // templateUrl: './app.component.html',
   template: `
     <h1>Hello, {{ title }}</h1>
+
+    <button
+      [routerLink]="['/basic/counter']"
+      routerLinkActive="router-link-active"
+      [routerLinkActiveOptions]="{exact: true}"
+    >
+      baasic counter
+    </button>
+
+    <button
+      [routerLink]="['/basic/counter/30']"
+      routerLinkActive="router-link-active"
+      [routerLinkActiveOptions]="{exact: true}"
+    >
+      baasic counter param
+    </button>
+
+    <button
+      [routerLink]="['/basic/pokemmon']"
+      routerLinkActive="router-link-active"
+    >
+      baasic request pokemon
+    </button>
+
+    <button
+      [routerLink]="['/basic/family']"
+      routerLinkActive="router-link-active"
+    >
+      baasic input output father son
+    </button>
+
+
     <router-outlet></router-outlet>
   `,
   styleUrl: './app.component.scss'
